11cab08cd20aaed5b2c829d2ac05a3112c2012cc
Dheeraz Satsangi <satsangi.dheeraz@gmail.com>
2022-12-05 16:47:56 +0530

    added sonar.yml

---
+++ Blink128a.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Blink128a.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Blink128a.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=blink128a.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/blink128a.x.tar
